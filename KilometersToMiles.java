0b53cbbab40a84024f1371b6f2f94b4b775f55c7
Sssrajputt <149288600+Sssrajputt@users.noreply.github.com>
2025-01-08 12:51:31 +0530

    Kilometers To Miles

---
+++ KilometersToMiles.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class KilometersToMiles{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the distance in kilometers: ");
+        double kilometers = sc.nextDouble();
+
+        double miles = kilometers * 0.621371;
+
+        System.out.println(kilometers + " kilometers is equal to " + miles + " miles.");
+
+        sc.close();
+    }
+}
