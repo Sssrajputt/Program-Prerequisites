ac888ab49678ea1577f4208549b28e7fda72fc6f
Sssrajputt <149288600+Sssrajputt@users.noreply.github.com>
2025-01-08 12:34:14 +0530

    Add Two Numbers

---
+++ AddTwoNumbers.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class AddTwoNumbers{
+public static void main (String args[]){
+
+Scanner sc = new Scanner(System.in);
+int num_1 = sc.nextInt();
+int num_2 = sc.nextInt();
+System.out.println(num_1+num_2);
+}
+}
+
